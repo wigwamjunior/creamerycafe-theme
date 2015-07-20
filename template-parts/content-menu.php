9588a6f0d3e95f1d90b7fb7789ecddb0654950db
wigwamjunior <wigwamjunior@users.noreply.github.com>
2015-07-20 09:25:16 -0500

    Removed hyperlink in blog post

--- template-parts/content-menu.php
+++ template-parts/content-menu.php
@@ -9,7 +9,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title">', esc_url( get_permalink() ) ), '</h3>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
