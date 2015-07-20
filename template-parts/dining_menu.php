f5de9ed1028306b34f77f411799ce63ce150164f
wigwamjunior <wigwamjunior@users.noreply.github.com>
2015-07-20 13:09:49 -0500

    Cleaned up directory and synced with live site

--- template-parts/dining_menu.php
+++ template-parts/dining_menu.php
@@ -1,245 +1,85 @@
 <?php
 /*
-Template Name: Menu Original
+Template Name: Menu
 */
 ?>
 
 <?php get_header(); ?>
-
-<?php list_child_pages(); ?>
-
-	<div class="container" class="content-area">
-		<main role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<div class="main-content">
-					<div class="row">
-						<div class="four columns">
-							<header>
-								<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-							</header><!-- .entry-header -->
-							<section>
-								<?php the_content(); ?>
-							</section>
-						</div>
-					</div>
+<div class="container" class="content-area">
+	<main role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
 					
-					<!-- Menu Day -->
-					<div>
-						<h2>Day</h2>
-						<hr>
-						<?php 
-						// check for rows
-						if( have_rows('eggs_day') ): ?>
-							<h3>Eggs</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('eggs_day') ): the_row();
-
-									// display each item
-									?>
-									
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('grains_day') ): ?>
-							<h3>Grains</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('grains_day') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('sandwiches_day') ): ?>
-							<h3>Sandwiches</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('sandwiches_day') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('plates_day') ): ?>
-							<h3>Plates</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('plates_day') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('desserts_day') ): ?>
-							<h3>Desserts</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('desserts_day') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-					</div>
-					<!-- END Menu Day -->
-				
-					<!-- Menu Evening -->
-					<div>
-						<h2>Evening</h2>
-						<hr>
-						<?php 
-						// check for rows
-						if( have_rows('salad_evening') ): ?>
-							<h3>Salads</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('salad_evening') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('small_plates') ): ?>
-							<h3>Small Plates</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('small_plates') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('plates_evening') ): ?>
-							<h3>Plates</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('plates_evening') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-						
-						<?php 
-						// check for rows
-						if( have_rows('desserts_evening') ): ?>
-							<h3>Desserts</h3>
-							<div class="row">
-								<?php 
-								// loop through rows
-								while( have_rows('desserts_evening') ): the_row();
-
-									// display each item
-									?>
-									<div class="two columns">
-										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
-										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
-										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
-									</div>
-								<?php endwhile; ?>
-							</div>
-						<?php endif; ?>
-					</div>
-					<!-- END Menu Evening -->
-							<div>
-							
-							<?php if ( is_active_sidebar( 'dining-menu' ) ) { ?>
-	<ul id="sidebar">
-		<?php dynamic_sidebar( 'dining-menu' ); ?>
-	</ul>
-<?php } ?>
-
-			</div>
-				
-				</div>
-				<!-- .entry-content -->
-
-			</article><!-- #post-## -->
-
+			<header>
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header>
 			
-			<?php endwhile; // End of the loop. ?>
+			<!-- LEAD content -->
+			<div class="main-content row">
+				<div class="six columns">
+					<section>
+						<?php the_content(); ?>
+					</section>
+				</div>
+			</div>
+			<!-- END LEAD content -->
+		<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
+		<nav class="inner-nav">
+			<ul>
+				<li><a href="#tab1">Day <span class="menu__hours">(8 a.m. to 4 p.m.)</span></a></li> <!-- Tabs correspond to categories that food group posts live in -->
+				<li><a href="#tab2">Evening <span class="menu__hours">(4 p.m. to close)</span></a></li> <!-- Tabs correspond to categories that food group posts live in -->
+			</ul>
+		</nav>
 		
-	</div>
+		<div id="tab1">
+			
+			<!-- Find the posts associated with the 'Day' category and display -->
+			<?php
+				$loop = new WP_Query( array( 'post_type' => 'menu', 'posts_per_page' => 20, 'category_name' => 'day' ) );
+				while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				
+				<?php
+					$format = get_post_format();
+					echo ($format);
+					/*
+					 * Fetch the content-menu template for display the food group posts
+					 */
+					get_template_part( 'template-parts/content', 'menu' );
+				?>
+				
+				<!-- Restore original Post Data -->
+				<?php wp_reset_postdata(); ?>
+				
+			<?php endwhile; ?>
+			
+		</div>
+		
+		<div id="tab2">
+			
+			<!-- Find the posts associated with the 'Day' category and display -->
+			<?php
+				$loop = new WP_Query( array( 'post_type' => 'menu', 'posts_per_page' => 20, 'category_name' => 'evening' ) );
+				while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				
+				<?php
+					$format = get_post_format();
+					echo ($format);
+					/*
+					 * Fetch the content-menu template for display the food group posts
+					 */
+					get_template_part( 'template-parts/content', 'menu' );
+				?>
+				
+				<!-- Restore original Post Data -->
+				<?php wp_reset_postdata(); ?>
+				
+			<?php endwhile; ?>
+			
+		</div>
+		
+	</main><!-- #main -->
+		
+</div>
 	<!-- END Container -->
 
 <?php get_footer(); ?>
---
+++ template-parts/dining_menu_old.php
@@ -0,0 +1,245 @@
+<?php
+/*
+Template Name: Menu Original
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php list_child_pages(); ?>
+
+	<div class="container" class="content-area">
+		<main role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="main-content">
+					<div class="row">
+						<div class="four columns">
+							<header>
+								<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+							</header><!-- .entry-header -->
+							<section>
+								<?php the_content(); ?>
+							</section>
+						</div>
+					</div>
+					
+					<!-- Menu Day -->
+					<div>
+						<h2>Day</h2>
+						<hr>
+						<?php 
+						// check for rows
+						if( have_rows('eggs_day') ): ?>
+							<h3>Eggs</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('eggs_day') ): the_row();
+
+									// display each item
+									?>
+									
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('grains_day') ): ?>
+							<h3>Grains</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('grains_day') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('sandwiches_day') ): ?>
+							<h3>Sandwiches</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('sandwiches_day') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('plates_day') ): ?>
+							<h3>Plates</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('plates_day') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('desserts_day') ): ?>
+							<h3>Desserts</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('desserts_day') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+					</div>
+					<!-- END Menu Day -->
+				
+					<!-- Menu Evening -->
+					<div>
+						<h2>Evening</h2>
+						<hr>
+						<?php 
+						// check for rows
+						if( have_rows('salad_evening') ): ?>
+							<h3>Salads</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('salad_evening') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('small_plates') ): ?>
+							<h3>Small Plates</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('small_plates') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('plates_evening') ): ?>
+							<h3>Plates</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('plates_evening') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php 
+						// check for rows
+						if( have_rows('desserts_evening') ): ?>
+							<h3>Desserts</h3>
+							<div class="row">
+								<?php 
+								// loop through rows
+								while( have_rows('desserts_evening') ): the_row();
+
+									// display each item
+									?>
+									<div class="two columns">
+										<strong class="menu__item-name"><?php the_sub_field('item_name'); ?></strong>
+										<p class="menu__item-copy"><?php the_sub_field('item_description'); ?></p>
+										<span class="menu__item-price"><?php the_sub_field('item_price'); ?></span>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+					</div>
+					<!-- END Menu Evening -->
+							<div>
+							
+							<?php if ( is_active_sidebar( 'dining-menu' ) ) { ?>
+	<ul id="sidebar">
+		<?php dynamic_sidebar( 'dining-menu' ); ?>
+	</ul>
+<?php } ?>
+
+			</div>
+				
+				</div>
+				<!-- .entry-content -->
+
+			</article><!-- #post-## -->
+
+			
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+		
+	</div>
+	<!-- END Container -->
+
+<?php get_footer(); ?>
