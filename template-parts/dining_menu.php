84accdd96e00f7a440dfb31b52700e1d3136adc6
wigwamjunior <wigwamjunior@users.noreply.github.com>
2015-08-05 13:02:59 -0500

    Update to include "beverages" tab

--- template-parts/dining_menu.php
+++ template-parts/dining_menu.php
@@ -28,6 +28,7 @@ Template Name: Menu
 			<ul>
 				<li><a href="#tab1">Day <span class="menu__hours">(8 a.m. to 4 p.m.)</span></a></li> <!-- Tabs correspond to categories that food group posts live in -->
 				<li><a href="#tab2">Evening <span class="menu__hours">(4 p.m. to close)</span></a></li> <!-- Tabs correspond to categories that food group posts live in -->
+				<li><a href="#tab3">Beverages</a></li> <!-- Tabs correspond to categories that food group posts live in -->
 			</ul>
 		</nav>
 		
@@ -35,7 +36,7 @@ Template Name: Menu
 			
 			<!-- Find the posts associated with the 'Day' category and display -->
 			<?php
-				$loop = new WP_Query( array( 'post_type' => 'menu', 'posts_per_page' => 20, 'category_name' => 'day' ) );
+				$loop = new WP_Query( array( 'post_type' => 'food_group', 'posts_per_page' => 20, 'category_name' => 'day' ) );
 				while ( $loop->have_posts() ) : $loop->the_post(); ?>
 				
 				<?php
@@ -58,7 +59,30 @@ Template Name: Menu
 			
 			<!-- Find the posts associated with the 'Day' category and display -->
 			<?php
-				$loop = new WP_Query( array( 'post_type' => 'menu', 'posts_per_page' => 20, 'category_name' => 'evening' ) );
+				$loop = new WP_Query( array( 'post_type' => 'food_group', 'posts_per_page' => 20, 'category_name' => 'evening' ) );
+				while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				
+				<?php
+					$format = get_post_format();
+					echo ($format);
+					/*
+					 * Fetch the content-menu template for display the food group posts
+					 */
+					get_template_part( 'template-parts/content', 'menu' );
+				?>
+				
+				<!-- Restore original Post Data -->
+				<?php wp_reset_postdata(); ?>
+				
+			<?php endwhile; ?>
+			
+		</div>
+		
+		<div id="tab3">
+			
+			<!-- Find the posts associated with the 'Day' category and display -->
+			<?php
+				$loop = new WP_Query( array( 'post_type' => 'food_group', 'posts_per_page' => 20, 'category_name' => 'beverages' ) );
 				while ( $loop->have_posts() ) : $loop->the_post(); ?>
 				
 				<?php
